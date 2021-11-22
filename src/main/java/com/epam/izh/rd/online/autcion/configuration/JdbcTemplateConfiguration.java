5d2aab3b457168c9d72c18b39406e7a3f1271a1c
Evgeniy <nifpopnaf@gmail.com>
2021-11-22 11:59:38 +0400

    test passed

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -1,21 +1,45 @@
 package com.epam.izh.rd.online.autcion.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class JdbcTemplateConfiguration {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String userPassword;
+
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return null;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
+
 
     @Bean
     public DataSource dataSource() {
-        return null;
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(userPassword);
+
+        return dataSource;
     }
 }
--- src/main/java/com/epam/izh/rd/online/autcion/repository/JdbcTemplatePublicAuction.java
+++ src/main/java/com/epam/izh/rd/online/autcion/repository/JdbcTemplatePublicAuction.java
@@ -3,67 +3,161 @@ package com.epam.izh.rd.online.autcion.repository;
 import com.epam.izh.rd.online.autcion.entity.Bid;
 import com.epam.izh.rd.online.autcion.entity.Item;
 import com.epam.izh.rd.online.autcion.entity.User;
+import com.epam.izh.rd.online.autcion.mappers.BidMapper;
+import com.epam.izh.rd.online.autcion.mappers.ItemMapper;
+import com.epam.izh.rd.online.autcion.mappers.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
-
+@Repository
 public class JdbcTemplatePublicAuction implements PublicAuction {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    BidMapper bidMapper;
+
+    @Autowired
+    ItemMapper itemMapper;
+
+    @Autowired
+    UserMapper userMapper;
 
     @Override
     public List<Bid> getUserBids(long id) {
-        return emptyList();
+
+        String sqlQuery = "select * from bids where user_id = ?;";
+
+        return jdbcTemplate.query(sqlQuery, new Object[]{id}, bidMapper);
     }
 
     @Override
     public List<Item> getUserItems(long id) {
-        return emptyList();
+
+        String sqlQuery = "select * from items where user_id = ?;";
+        return jdbcTemplate.query(sqlQuery, new Object[]{id}, itemMapper);
     }
 
     @Override
     public Item getItemByName(String name) {
-        return new Item();
+
+        String sqlQuery = "select * from items where title like ? limit 1;";
+        return jdbcTemplate.queryForObject(sqlQuery, new Object[]{"%" + name + "%"}, itemMapper);
     }
 
     @Override
     public Item getItemByDescription(String name) {
-        return new Item();
+
+        String sqlQuery = "select * from items where description like ? limit 1;";
+        return jdbcTemplate.queryForObject(sqlQuery, new Object[]{"%" + name + "%"}, itemMapper);
     }
 
     @Override
     public Map<User, Double> getAvgItemCost() {
-        return emptyMap();
+
+        Map<User, Double> mapUserAvgItem = new TreeMap<>((o1, o2) -> o1.getUserId().compareTo(o2.getUserId()));
+        String sqlQueryAllUsers = "select * from users;";
+        List<User> listUser = jdbcTemplate.query(sqlQueryAllUsers, userMapper);
+
+        for (User user : listUser) {
+            String sqlQueryAvgCostByUser = "select AVG(items.start_price) from items where items.user_id = ?";
+            Double valueAvgOfCost = jdbcTemplate.queryForObject(sqlQueryAvgCostByUser, new Object[]{user.getUserId()}, Double.class);
+            if (valueAvgOfCost != null) {
+                mapUserAvgItem.put(user, valueAvgOfCost);
+            }
+        }
+        return mapUserAvgItem;
     }
 
     @Override
     public Map<Item, Bid> getMaxBidsForEveryItem() {
-        return emptyMap();
+        String sqlQueryForMaxBidsForEveryItem = "select * from " +
+                "bids inner join items " +
+                "on items.item_id = bids.item_id " +
+                "where bids.bid_value = " +
+                "(select max(bids.bid_value) " +
+                "from bids where bids.item_id = items.item_id) " +
+                "order by items.item_id;";
+
+        return jdbcTemplate.query(sqlQueryForMaxBidsForEveryItem, rs -> {
+            Map<Item, Bid> mapItemByBid = new TreeMap<>((o1, o2) -> o1.getItemId().compareTo(o2.getItemId()));
+
+            int i = 0;
+            while (rs.next()) {
+                mapItemByBid.put(itemMapper.mapRow(rs, i), bidMapper.mapRow(rs, i));
+                i++;
+            }
+            return mapItemByBid;
+        });
     }
 
+    /**
+     * Добавить нового пользователя
+     */
     @Override
     public boolean createUser(User user) {
-        return false;
+        String sqlQuery = "insert into users values (?, ?, ?, ?, ?);";
+
+        return jdbcTemplate.update(
+                sqlQuery,
+                user.getUserId(),
+                user.getBillingAddress(),
+                user.getFullName(),
+                user.getLogin(), user.getPassword()) > 0;
     }
 
     @Override
     public boolean createItem(Item item) {
-        return false;
+        String sqlQuery = "insert into items values (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        return jdbcTemplate.update(
+                sqlQuery,
+                item.getItemId(),
+                item.getBidIncrement(),
+                item.getBuyItNow(),
+                item.getDescription(),
+                item.getStartDate(),
+                item.getStartPrice(),
+                item.getStopDate(),
+                item.getTitle(),
+                item.getUserId()) > 0;
     }
 
+    /**
+     * Добавить новую ставку
+     */
     @Override
     public boolean createBid(Bid bid) {
-        return false;
+        String sqlQuery = "insert into bids values (?, ?, ?, ?, ?);";
+        return jdbcTemplate.update(
+                sqlQuery,
+                bid.getBidId(),
+                bid.getBidDate(),
+                bid.getBidValue(),
+                bid.getItemId(),
+                bid.getUserId()) > 0;
     }
 
+    /**
+     * Увеличить стартовые цены товаров данного пользователя вдвое
+     */
     @Override
     public boolean deleteUserBids(long id) {
-        return false;
+        String sqlQuery = "delete from bids where user_id = ?;";
+        return jdbcTemplate.update(sqlQuery, id) > 0;
     }
 
+    /**
+     * Увеличить стартовые цены товаров данного пользователя вдвое
+     */
     @Override
     public boolean doubleItemsStartPrice(long id) {
-        return false;
+        String sqlQuery = "update items as i " +
+                "set i.start_price = i.start_price*2 " +
+                "where i.user_id = ? ;";
+        return jdbcTemplate.update(sqlQuery, id) > 0;
     }
 }

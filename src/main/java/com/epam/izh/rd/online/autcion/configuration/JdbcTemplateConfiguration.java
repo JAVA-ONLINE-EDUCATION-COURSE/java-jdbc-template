189100f7e88b18e0b99d410924152f6e82709ebe
Anliks <axel_lines@mail.ru>
2022-04-05 19:58:43 +0300

    done

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -1,21 +1,44 @@
 package com.epam.izh.rd.online.autcion.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+
 
 import javax.sql.DataSource;
 
+
 @Configuration
+@PropertySource("classpath:application.properties")
 public class JdbcTemplateConfiguration {
 
+    @Value("${spring.datasource.driverClassName}")
+    String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return null;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
     @Bean
     public DataSource dataSource() {
-        return null;
+        DataSourceBuilder ds = DataSourceBuilder.create();
+        ds.driverClassName(driverClassName);
+        ds.url(url);
+        ds.username(username);
+        ds.password(password);
+        return ds.build();
     }
 }
--- src/main/java/com/epam/izh/rd/online/autcion/mappers/ItemMapper.java
+++ src/main/java/com/epam/izh/rd/online/autcion/mappers/ItemMapper.java
@@ -2,14 +2,25 @@ package com.epam.izh.rd.online.autcion.mappers;
 
 import com.epam.izh.rd.online.autcion.entity.Item;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+@Component
 public class ItemMapper implements RowMapper<Item> {
 
     @Override
     public Item mapRow(ResultSet resultSet, int i) throws SQLException {
-        return new Item();
+         Item item = new Item();
+         item.setItemId(resultSet.getLong("item_id"));
+         item.setTitle(resultSet.getString("title"));
+         item.setDescription(resultSet.getString("description"));
+         item.setStartPrice(resultSet.getDouble("start_price"));
+         item.setBidIncrement(resultSet.getDouble("bid_increment"));
+         item.setStartDate(resultSet.getDate("start_date").toLocalDate());
+         item.setStopDate(resultSet.getDate("stop_date").toLocalDate());
+         item.setBuyItNow(resultSet.getBoolean("buy_it_now"));
+         item.setUserId(resultSet.getLong("user_id"));
+        return item;
     }
 }

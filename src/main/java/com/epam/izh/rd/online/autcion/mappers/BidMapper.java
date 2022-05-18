f518b922d13a197721e116270eb530eb1e91e755
serg1971ch12@gmail.com <https://gitlab.skillbox.ru>
2022-05-18 06:25:31 +0500

    Это задание по изучению JDBC Spring по-русски

--- src/main/java/com/epam/izh/rd/online/autcion/mappers/BidMapper.java
+++ src/main/java/com/epam/izh/rd/online/autcion/mappers/BidMapper.java
@@ -5,11 +5,22 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class BidMapper implements RowMapper<Bid> {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+
 
     @Override
-    public Bid mapRow(ResultSet resultSet, int i) throws SQLException {
-        return new Bid();
+    public Bid mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+
+        Bid bid = new Bid();
+        bid.setBidId(resultSet.getLong("BID_ID"));
+        bid.setBidDate(resultSet.getDate("BID_DATE").toLocalDate());
+        bid.setBidValue(resultSet.getDouble("BID_VALUE"));
+        bid.setItemId(resultSet.getLong("ITEM_ID"));
+        bid.setUserId(resultSet.getLong("USER_ID"));
+        return bid;
     }
 }
